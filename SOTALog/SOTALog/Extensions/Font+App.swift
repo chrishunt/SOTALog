231db7b91dd3828c6cf975800c40071725a6dc20
Chris Hunt <huntca@gmail.com>
2026-03-03 20:01:42 -0800

    Increase small font tokens from caption to footnote
    - Bump appTimestamp, appProgress, appDistanceLabel, appReferenceCode,
      and appLabel from Font.caption to Font.footnote for better readability

--- SOTALog/SOTALog/Extensions/Font+App.swift
+++ SOTALog/SOTALog/Extensions/Font+App.swift
@@ -6,11 +6,11 @@ extension Font {
     static let appFrequency = Font.subheadline.monospacedDigit()
     static let appSectionHeader = Font.title3.monospacedDigit().bold()
     static let appNumberKey = Font.title3.monospacedDigit()
-    static let appTimestamp = Font.caption.monospacedDigit()
-    static let appProgress = Font.caption.monospacedDigit().bold()
-    static let appDistanceLabel = Font.caption.monospacedDigit()
-    static let appReferenceCode = Font.caption.monospaced()
-    static let appLabel = Font.caption
+    static let appTimestamp = Font.footnote.monospacedDigit()
+    static let appProgress = Font.footnote.monospacedDigit().bold()
+    static let appDistanceLabel = Font.footnote.monospacedDigit()
+    static let appReferenceCode = Font.footnote.monospaced()
+    static let appLabel = Font.footnote
     static let appBadgeSmall = Font.caption2.bold()
     static let appMetadataLine1 = Font.callout.monospacedDigit()
     static let appMetadataLine2 = Font.callout
